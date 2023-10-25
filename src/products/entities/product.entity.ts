916166a358f35cd2ef63451975038eb26db013a2
Alonso Salcido <warmab13@Alonsos-MacBook-Pro-2.local>
2023-10-25 16:59:20 -0600

    adding images tags and create new entity for images, also refactor post to make relations between creating product and new entity product-image.entity.ts

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from "../entities";
 
 @Entity()
 export class Product {
@@ -49,6 +50,12 @@ export class Product {
 
 
     //images
+    @OneToMany(
+        ()=> ProductImage,
+        (productImage) => productImage.product,
+        { cascade: true } 
+    )
+    images?: ProductImage[];
 
 
     @BeforeInsert()
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,6 +6,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PaginationDto } from '../common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid';
+import { ProductImage } from './entities';
 
 @Injectable()
 export class ProductsService {
@@ -14,8 +15,10 @@ export class ProductsService {
 
   constructor(
     @InjectRepository(Product)
-
     private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>,
 
   ){}
 
@@ -23,23 +26,15 @@ export class ProductsService {
 
     try {
 
-      // if( !createProductDto.slug ){
-      //   createProductDto.slug = createProductDto.title
-      //     .toLowerCase()
-      //     .replaceAll(' ', '_')
-      //     .replaceAll("'", '')
-      // }else{
-      //   createProductDto.slug = createProductDto.slug
-      //     .toLowerCase()
-      //     .replaceAll(' ', '_')
-      //     .replaceAll("'", '')
-      // }
+      const { images = [], ...productDetails } = createProductDto;
 
-      //Just creating instance from Product
-      const product = this.productRepository.create(createProductDto);
+      const product = this.productRepository.create({
+        ...productDetails,
+        images: images.map( image => this.productImageRepository.create({ url: image }) )
+      });
       await this.productRepository.save( product );
       
-      return product;
+      return { ...product, images };
 
     } catch (error) {
       this.handleDbExceptions(error);
@@ -81,7 +76,8 @@ export class ProductsService {
   async update(id: string, updateProductDto: UpdateProductDto) {
     const product = await this.productRepository.preload({
       id: id,
-      ...updateProductDto
+      ...updateProductDto,
+      images: [],
     })
 
     if(!product) throw new NotFoundException(`Product with id ${id} not found`);
