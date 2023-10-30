91a70f1a8cbd1743165caf59d58ea08ca4ea0412
Alonso Salcido <warmab13@Alonsos-MacBook-Pro-2.local>
2023-10-30 16:01:50 -0600

    using query runner to make rollback and use to manage transactions

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,7 +3,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { PaginationDto } from '../common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid';
 import { ProductImage } from './entities';
@@ -19,6 +19,8 @@ export class ProductsService {
 
     @InjectRepository(ProductImage)
     private readonly productImageRepository: Repository<ProductImage>,
+
+    private readonly dataSource: DataSource,
 
   ){}
 
@@ -91,18 +93,42 @@ export class ProductsService {
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
-    const product = await this.productRepository.preload({
-      id: id,
-      ...updateProductDto,
-      images: [],
-    })
+
+    //Product preload
+
+    const { images, ...toUpdate } =  updateProductDto;
+    const product = await this.productRepository.preload({ id, ...toUpdate })
 
     if(!product) throw new NotFoundException(`Product with id ${id} not found`);
 
+    // Create query runner
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
-      await this.productRepository.save(product);  
-      return product;
+
+      if( images ){
+        await queryRunner.manager.delete( ProductImage, { product: { id } } );
+
+        product.images = images.map( 
+          image => this.productImageRepository.create({ url: image }) 
+        )
+      }
+
+      await queryRunner.manager.save( product );
+
+      //await this.productRepository.save(product);  
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+      
+      return this.findOnePlain( id );
+
     } catch (error) {
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
+
       this.handleDbExceptions(error)
     }
 
